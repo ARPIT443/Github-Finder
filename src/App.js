0448cffa7caca4ca68c7924f7e967b67b32b6892
Arpit Khandelwal <arpit443@gmail.com>
2021-01-31 00:04:42 +0530

    Add Search bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
 
 class App extends Component {
@@ -26,7 +27,8 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className='container'>
-          <Users users={this.state.users} isloading={this.state.isLoading}/>
+          <Search />
+          <Users users={this.state.users} isLoading={this.state.isLoading}/>
         </div>
       </div>
     );
